c90618781280d50b47ab389e2b922f2a2085666d
Mikey <mknox2003@icloud.com>
2025-06-15 22:32:26 +0100

    Moved to dynamic app configs to support .env files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,14 +23,14 @@ export default function RootLayout() {
 	}
 
 	return (
-		<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-			<AuthProvider>
+		<AuthProvider>
+			<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
 				<Stack>
 					<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 					<Stack.Screen name="+not-found" />
 				</Stack>
 				<StatusBar style="auto" />
-			</AuthProvider>
-		</ThemeProvider>
+			</ThemeProvider>
+		</AuthProvider>
 	);
 }
