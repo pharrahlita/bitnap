dfc97e06564a49b5aa6aae1de6c23f1e1f5bffc7
Mikey <michael.knox@talktalk.net>
2025-06-25 21:44:08 +0100

    Add secondary color and update primary color values
    Introduced a new 'secondary' color and updated the 'primary' color value for both light and dark themes in Colors.ts to improve UI consistency.

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -14,7 +14,8 @@ export const Colors = {
 		icon: '#687076',
 		tabIconDefault: '#687076',
 		tabIconSelected: tintColorLight,
-		primary: '#0a7ea4',
+		primary: '#a06ffc',
+		secondary: '#d1c87f',
 	},
 	dark: {
 		text: '#ECEDEE',
@@ -23,6 +24,7 @@ export const Colors = {
 		icon: '#9BA1A6',
 		tabIconDefault: '#9BA1A6',
 		tabIconSelected: tintColorDark,
-		primary: '#0a7ea4',
+		primary: '#a06ffc',
+		secondary: '#d1c87f',
 	},
 };
