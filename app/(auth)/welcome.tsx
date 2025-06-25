242861164fe6575447de4fe843243a9590f26dad
Mikey <michael.knox@talktalk.net>
2025-06-25 22:26:37 +0100

    Update welcome screen UI and branding
    Added a high-resolution logo image, updated colors, font, and button styles to improve the visual appearance and align with new branding. Adjusted text, background color, and font sizes for a refreshed look.

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,18 +1,23 @@
 import { Colors } from '@/constants/Colors';
 import { useRouter } from 'expo-router';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function WelcomeScreen() {
 	const router = useRouter();
 
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>bitnap</Text>
+			<Image
+				source={require('@/assets/images/bitnap_highres_logo.png')}
+				style={{ width: 200, height: 200, marginBottom: 25 }}
+				resizeMode="contain"
+			/>
+			<Text style={styles.title}>bitnap.</Text>
 			<TouchableOpacity
 				style={styles.buttonOutlined}
 				onPress={() => router.push('/login')}
 			>
-				<Text style={[styles.buttonText, { color: Colors.dark.primary }]}>
+				<Text style={[styles.buttonText, { color: Colors.dark.secondary }]}>
 					Sign In
 				</Text>
 			</TouchableOpacity>
@@ -34,30 +39,31 @@ const styles = StyleSheet.create({
 		padding: 24,
 		paddingTop: 200,
 		paddingBottom: 200,
-		backgroundColor: '#1c1c1c',
+		backgroundColor: '#3E3748',
 	},
 	title: {
-		fontSize: 28,
+		fontSize: 48,
 		fontWeight: 'bold',
-		color: Colors.dark.primary,
+		fontFamily: 'PixelifySans_Bold',
+		color: Colors.dark.text,
 		marginBottom: 40,
 	},
 	buttonOutlined: {
 		backgroundColor: 'transparent',
-		borderColor: Colors.dark.primary,
+		borderColor: Colors.dark.secondary,
 		borderWidth: 2,
 		paddingVertical: 12,
 		paddingHorizontal: 24,
-		borderRadius: 25,
+		borderRadius: 8,
 		marginVertical: 10,
 		width: '75%',
 		alignItems: 'center',
 	},
 	buttonFilled: {
-		backgroundColor: Colors.dark.primary,
+		backgroundColor: Colors.dark.secondary,
 		paddingVertical: 12,
 		paddingHorizontal: 24,
-		borderRadius: 25,
+		borderRadius: 8,
 		marginVertical: 10,
 		width: '75%',
 		alignItems: 'center',
