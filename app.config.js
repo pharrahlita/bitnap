03789d1180b407818b48feb5ab72926bd4e45a9d
Mikey <michael.knox@talktalk.net>
2025-06-27 21:59:15 +0100

    eas & build setup

--- app.config.js
+++ app.config.js
@@ -11,8 +11,10 @@ export default {
 		userInterfaceStyle: 'automatic',
 		newArchEnabled: true,
 		ios: {
+			bundleIdentifier: 'com.nuhm.bitnap',
 			supportsTablet: true,
 			infoPlist: {
+				ITSAppUsesNonExemptEncryption: false,
 				NSPhotoLibraryUsageDescription:
 					'Allow Bitnap to access your photos for profile picture uploads.',
 			},
@@ -47,6 +49,9 @@ export default {
 		extra: {
 			supabaseUrl: process.env.SUPABASE_URL,
 			supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+			eas: {
+				projectId: 'b08b2ca9-d3fe-4f2b-8a27-7c591dfe0cc5',
+			},
 		},
 	},
 };
